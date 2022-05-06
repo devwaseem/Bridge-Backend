e91a3d57621ed92098dff08ef9a2616fcfe5ecc3
Waseem akram <waseem07799@gmail.com>
2022-05-07 03:02:34 +0530

    Add Admin permissions for api docs

--- bridge/users/tests/test_swagger.py
+++ bridge/users/tests/test_swagger.py
@@ -1,5 +1,8 @@
 import pytest
 from django.urls import reverse
+from mixer.backend.django import mixer
+
+from bridge.users.models import User
 
 pytestmark = pytest.mark.django_db
 
@@ -12,8 +15,16 @@ def test_swagger_accessible_by_admin(admin_client):
 
 def test_swagger_ui_not_accessible_by_normal_user(client):
     url = reverse("api-docs")
+    user = mixer.blend(User, role=(User.Role.CANDIDATE, User.Role.RECRUITER))
+    client.force_login(user)
     response = client.get(url)
     assert response.status_code == 403
+
+
+def test_swagger_ui_not_accessible_by_public(client):
+    url = reverse("api-docs")
+    response = client.get(url)
+    assert response.status_code == 401
 
 
 def test_api_schema_generated_successfully(admin_client):
--- config/settings/base.py
+++ config/settings/base.py
@@ -250,7 +250,8 @@ LOGGING = {
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
