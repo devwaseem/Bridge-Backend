f1ed01e4912611be0b2678d4a55faf5a760bac74
Waseem akram <waseem07799@gmail.com>
2022-05-07 17:55:22 +0530

    Revert "Add Admin permissions for api docs"
    This reverts commit e91a3d57621ed92098dff08ef9a2616fcfe5ecc3.

--- config/settings/base.py
+++ config/settings/base.py
@@ -250,8 +250,7 @@ LOGGING = {
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
